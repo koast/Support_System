5e98e2d20db9e0f7a010fb762d7d461be7e6bd66
Alejandro Aller <Alejandroallerdiez@gmail.com>
2014-03-21 13:04:10 +0100

    Modificado constructor de la clase SupportSystem para que se pueda introducir bye de todas las maneras posibles

--- SupportSystem.java
+++ SupportSystem.java
@@ -38,7 +38,7 @@ public class SupportSystem
 
         while(!finished) {
             String input = reader.getInput();
-            if(input.trim().startsWith("bye"))
+            if(input.trim().toLowerCase().startsWith("bye"))
                 {
                 finished = true;
             }
