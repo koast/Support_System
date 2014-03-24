29a40cff18a469c3a31d55b3d3f6405da94eee88
Alejandro Aller <Alejandroallerdiez@gmail.com>
2014-03-24 09:15:02 +0100

    Modificada clase responder para que genere y returne varias frases aleatorias

--- Responder.java
+++ Responder.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +10,21 @@
  */
 public class Responder
 {
+    private Random aleatorio;
+    private ArrayList<String> frases;
+    
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        aleatorio = new Random();
+        frases = new ArrayList<>();
+        frases.add("That sounds interesting. Tell me more...");
+        frases.add("This will be important, i noting that...");
+        frases.add("Wait a moment, i need to ask about of this to other department");
+        frases.add("The technical service don't take care about that sorry");
+        frases.add("Sorry, I don't understand you, can u explain it in other way?");
     }
 
     /**
@@ -20,6 +33,6 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        return frases.get(aleatorio.nextInt(5));        
     }
 }
